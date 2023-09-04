db9c370b00128a86084807b0b678ce45b0d65f34
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-09-04 10:07:24 +0200

    resuelto, y seguir con el tuto

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,5 +1,13 @@
 import query from "@/components/query";
 import { digimon } from "@/interfaces/digimon";
+import Link from "next/link";
+
+const ToDetail = ({digimon}:{digimon:digimon}) =>
+    <Link
+        href={`/detail/${digimon.name}`}
+    >
+        <button type="button" className="btn btn-secondary m-1"> { digimon.name } </button>
+    </Link>
 
 const main = async() => {
 
@@ -7,7 +15,7 @@ const main = async() => {
 
     return  <div>
                 <h3> Para entrar en detalle : </h3>
-                { digimon.slice(0,10).map( (x,i) => <p key={i}>{x.name}</p> ) }
+                { digimon.slice(0,10).map( (x,i) => <ToDetail key={i} digimon={x}/> ) }
             </div> ;
 
 }
